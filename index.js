a279df23dea621236d8e90cd03c2a3172aadd060
Dan Passaro <danpassaro@gmail.com>
2017-01-14 04:03:31 -0500

    Redeploy by POSTing to :7001/redeploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var child_process = require('child_process')
 var fs = require('fs')
 var http = require('http')
 
+var express = require('express')
 var httpProxy = require('http-proxy')
 var portfinder = require('portfinder')
 
@@ -18,11 +19,59 @@ proxy.on('error', (err, req, res) => {
   res.end("Can't proxy request: " + err)
 })
 
+
+function launchBackend() {
+  var old_backend = server_process
+  portfinder.getPort((err, port) => {
+    if (err) {
+      console.error("Couldn't find a port:", err)
+      process.exit(1)
+    }
+
+    server_port = port
+    var self = child_process.exec(
+      config.run + ' ' + server_port, {}, (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error running the backend:', error)
+          process.exit(1)
+        } else if (!self.time_to_die) {
+          console.error('Backend exited abnormally. Stdout:')
+          console.error(stdout)
+          console.error('Stderr:')
+          console.error(stderr)
+          process.exit(1)
+        } else {
+          console.info(self.pid + ' killed')
+        }
+      }
+    )
+    server_process = self
+
+    if (old_backend !== null) {
+
+      // TODO: check it actually dies (this sends SIGTERM)
+      old_backend.time_to_die = true
+      old_backend.kill()
+      console.info('Killing', old_backend.pid)
+    }
+  })
+}
+
+
 var server = http.createServer((req, res) => {
   if (server_port === null) {
     throw "The port is unkonwn (backend is most likely not running)"
   }
   proxy.web(req, res, {target: 'http://localhost:' + server_port})
+})
+
+var controlServer = express()
+controlServer.get('/', (req, res) => {
+  res.send('Current backend port: ' + server_port + '\n')
+})
+controlServer.post('/redeploy/', (req, res) => {
+  res.end('Redeploying...\n')
+  launchBackend()
 })
 
 fs.readFile(config_path, null,  (err, data) => {
@@ -37,28 +86,7 @@ fs.readFile(config_path, null,  (err, data) => {
     process.exit(1)
   }
 
-  portfinder.getPort((err, port) => {
-    if (err) {
-      console.error("Couldn't find a port:", err)
-      process.exit(1)
-    }
-
-    server_port = port
-    server_process = child_process.exec(
-      config.run + ' ' + server_port, {}, (error, stdout, stderr) => {
-        if (error) {
-          console.error('Error running the backend:', error)
-          process.exit(1)
-        } else {
-          console.error('Backend exited abnormally. Stdout:')
-          console.error(stdout)
-          console.error('Stderr:')
-          console.error(stderr)
-          process.exit(1)
-        }
-      }
-    )
-  })
-
+  launchBackend()
   server.listen(7000)
+  controlServer.listen(7001)
 })
--- sample/sample.py
+++ sample/sample.py
@@ -1,6 +1,8 @@
 import datetime
+import time
 
 import flask
+from flask import Response
 app = flask.Flask(__name__)
 
 
@@ -10,3 +12,15 @@ now = datetime.datetime.now()
 @app.route('/')
 def index():
     return "Hello! This spawned at %s" % str(now)
+
+
+@app.route('/slow')
+def slow():
+    def doit():
+        yield "Hello! This spawned at %s<br>" % str(now)
+
+        # Last as long as Gunicorn timeout allows
+        for n in range(1, 31):
+            time.sleep(1)
+            yield "%s...<br>" % n
+    return Response(doit())
